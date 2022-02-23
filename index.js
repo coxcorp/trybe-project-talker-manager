df7077c9bc368bd57dd8dd031756c52709b898bd
Diego Silva <coxcorp@gmail.com>
2022-02-22 23:05:57 -0300

    requisito 01

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const FILENAME = 'talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const data = await fs.readFile(FILENAME);
+  res.status(HTTP_OK_STATUS).json(JSON.parse(data));
 });
 
 app.listen(PORT, () => {
